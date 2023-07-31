d3f4508842b0a1aca042651bf84960aa51ea4769
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-07-31 19:50:55 +0400

    first upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { styled } from "styled-components";
+export default function Header() {
+  return <Headmain>Hesader</Headmain>;
+}
+const Headmain = styled.div`
+  background-color: #393e46;
+`;

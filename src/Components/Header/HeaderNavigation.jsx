1cdae7ccfd8b2dbb757336b450c7fe1ef506ed70
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-08-01 19:05:06 +0400

    carusell

--- src/Components/Header/HeaderNavigation.jsx
+++ src/Components/Header/HeaderNavigation.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { styled } from "styled-components";
 import { DownOutlined } from "@ant-design/icons";
-import { Dropdown, message, Space } from "antd";
+import { Dropdown, Space } from "antd";
+import { Turn as Hamburger } from "hamburger-react";
+import { useState } from "react";
 export default function HeaderNavigation() {
+  const [isOpen, setOpen] = useState(false);
   const items = [
     {
       label: "1st menu item",
@@ -19,64 +22,79 @@ export default function HeaderNavigation() {
   ];
   return (
     <Nav>
-      <a className="navtext" href="">
-        მთავარი
-      </a>
-      <Dropdown
-        menu={{
-          items,
-        }}
-      >
-        <a onClick={(e) => e.preventDefault()}>
-          <Space>
-            <p className="navtext">ინსტრუმენტები</p>
-            <DownOutlined style={{ color: "#ffffff" }} />
-          </Space>
+      <div className="classic">
+        <a className="navtext" href="">
+          მთავარი
         </a>
-      </Dropdown>
-      <Dropdown
-        menu={{
-          items,
-        }}
-      >
-        <a onClick={(e) => e.preventDefault()}>
-          <Space>
-            <p className="navtext">სახლი და ეზო</p>
-            <DownOutlined style={{ color: "#ffffff" }} />
-          </Space>
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>
+              <p className="navtext">ინსტრუმენტები</p>
+              <DownOutlined style={{ color: "#ffffff" }} />
+            </Space>
+          </a>
+        </Dropdown>
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>
+              <p className="navtext">სახლი და ეზო</p>
+              <DownOutlined style={{ color: "#ffffff" }} />
+            </Space>
+          </a>
+        </Dropdown>
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>
+              <p className="navtext">ავტო ტექნიკა</p>
+              <DownOutlined style={{ color: "#ffffff" }} />
+            </Space>
+          </a>
+        </Dropdown>
+        <a className="navtext" href="">
+          ჩვენს შესახებ
         </a>
-      </Dropdown>
-      <Dropdown
-        menu={{
-          items,
-        }}
-      >
-        <a onClick={(e) => e.preventDefault()}>
-          <Space>
-            <p className="navtext">ავტო ტექნიკა</p>
-            <DownOutlined style={{ color: "#ffffff" }} />
-          </Space>
+        <a className="navtext" href="">
+          პირობები
         </a>
-      </Dropdown>
-      <a className="navtext" href="">
-        ჩვენს შესახებ
-      </a>
-      <a className="navtext" href="">
-        პირობები
-      </a>
-      <a className="navtext" href="">
-        კონტაქტი
-      </a>
+        <a className="navtext" href="">
+          კონტაქტი
+        </a>
+      </div>
+      <Ham>
+        <Hamburger
+          toggled={isOpen}
+          size={26}
+          toggle={setOpen}
+          color="#ffffff"
+        />
+      </Ham>
     </Nav>
   );
 }
 const Nav = styled.div`
   background-color: #393e46;
-  padding: 15px;
-  gap: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  .classic {
+    padding: 15px;
+    gap: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    @media screen and (max-width: 973px) {
+      display: none;
+    }
+  }
   .navtext {
     color: #ffffff;
     font-family: "Noto Sans Georgian", sans-serif;
@@ -84,5 +102,14 @@ const Nav = styled.div`
     font-weight: 400;
     line-height: 24px;
     text-decoration: none;
+  }
+`;
+const Ham = styled.div`
+  display: flex;
+  justify-content: end;
+  align-items: end;
+  padding: 3px;
+  @media screen and (min-width: 974px) {
+    display: none;
   }
 `;

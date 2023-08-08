ac2aeba30f6f82eb6bc7af5fb397f63dba3f367b
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-08-08 17:32:33 +0400

    update

--- src/Styled/SectionHeader.jsx
+++ src/Styled/SectionHeader.jsx
@@ -5,7 +5,7 @@ const SectionHeader = styled.div`
   font-size: 20px;
   font-weight: 700;
   line-height: 24px;
-  padding: 20px 0 0 50px;
+  padding: 20px 0 0 10px;
   margin: auto;
   max-width: 1200px;
 `;

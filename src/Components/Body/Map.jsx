14b979a4378034ef27386b75a3073a28b9f81f97
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-08-13 18:30:41 +0400

    fixing bugs and improvement

--- src/Components/Body/Map.jsx
+++ src/Components/Body/Map.jsx
@@ -1,46 +1,42 @@
-import React from "react";
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
-const containerStyle = {
-  width: "1200px",
-  height: "500px",
-  margin: "20px auto",
-};
-const center = {
-  lat: 41.68072,
-  lng: 44.9818,
-};
-function MyComponent() {
+import { React, useMemo } from "react";
+
+import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
+
+const Map = () => {
+  const containerStyle = {
+    width: "90%",
+    height: "500px",
+    margin: "20px auto",
+    maxWidth: "1200px",
+  };
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY,
   });
-  return isLoaded ? (
-    <>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={17}
-        mapContainerClassName="map-container"
-      >
-        <Marker
-          position={{
-            lat: 41.6,
-            lng: 44.98,
-          }}
-        />
-      </GoogleMap>
-      <GoogleMap mapContainerStyle={containerStyle} zoom={17} center={center}>
-        <Marker
-          position={{
-            lat: center.lat,
-            lng: center.lng,
-          }}
-        />
-      </GoogleMap>
-    </>
-  ) : (
-    <>Map Loading ...</>
-  );
-}
 
-export default React.memo(MyComponent);
+  const center = useMemo(() => ({ lat: 41.67994, lng: 44.98073 }), []);
+
+  const onLoad = (marker) => {
+    console.log("marker: ", marker);
+  };
+
+  const options = {
+    streetViewControl: false,
+  };
+
+  if (!isLoaded) return <div>Loading...</div>;
+
+  return (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      zoom={17}
+      options={options}
+      center={center}
+      mapContainerClassName="map-container"
+    >
+      <MarkerF onLoad={onLoad} position={center} />
+    </GoogleMap>
+  );
+};
+
+export default Map;

9f940a5915898873e60d24c3212d9f2a0b7a6642
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-08-23 14:10:15 +0400

    updated info

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import Home from "./Home";
 import Terms from "./Terms";
 import Contact from "./Contact";
-import Product from "./Product";
 import Tools from "./Tools";
-import Garden from "./Garden";
-import Cars from "./Cars";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
@@ -14,8 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tools/*" element={<Tools />} />
-        <Route path="/homeandgarden/*" element={<Garden />} />
-        <Route path="/cars/*" element={<Cars />} />
+        <Route path="/homeandgarden/*" element={<Tools />} />
+        <Route path="/cars/*" element={<Tools />} />
         <Route path="/terms" element={<Terms />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
--- src/Tools.jsx
+++ src/Tools.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Subcategory from "./Subcategory";
-import NotFound from "./NotFound"; // Your 404 component
 import Product from "./Product";
 
 const Tools = () => {
@@ -9,7 +8,6 @@ const Tools = () => {
     <Routes>
       <Route path=":subcategoryId/*" element={<Subcategory />} />
       <Route path="/" element={<Product />} />
-      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
